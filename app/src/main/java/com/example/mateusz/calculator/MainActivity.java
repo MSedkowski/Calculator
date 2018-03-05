06a0169a4c34fc2f86874dbc45979820b70fb6c1
Mateusz Sędkowski <mateusz.sedkowski@gmail.com>
2018-03-05 20:02:38 +0100

    Add about and start menu.

--- app/src/main/java/com/example/mateusz/calculator/MainActivity.java
+++ app/src/main/java/com/example/mateusz/calculator/MainActivity.java
@@ -1,174 +1,29 @@
 package com.example.mateusz.calculator;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView inputField;
-    private double valueOne = Double.NaN;
-    private double valueTwo;
-    private String operation = null;
-    private boolean clearFlag = false;
-    private boolean equationCalculated = true;
-
+    private boolean darkMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        inputField = findViewById(R.id.inputField);
-    }
-    @SuppressLint("SetTextI18n")
-    public void addDigit(View view) {
-        if(clearFlag) {
-            inputField.setText("");
-            clearFlag = false;
-        }
-        Button digit = (Button) view;
-        String buttonText = digit.getText().toString();
-        Integer value = Integer.parseInt(buttonText);
-        if(inputField.getText().length() != 0) {
-            String temp = inputField.getText().toString();
-            inputField.setText(temp + value.toString());
-        }
-        else {
-            inputField.setText(value.toString());
-        }
-        equationCalculated = false;
+        setContentView(R.layout.start_view);
     }
 
-    @SuppressLint("SetTextI18n")
-    public void makeEquation(View view) {
-        try {
-            Button equationSign = (Button) view;
-            if (equationSign.getText().toString().equals("=")) {
-                if (!equationCalculated) {
-                    valueTwo = Double.parseDouble(inputField.getText().toString());
-                }
-                equationCalculated = false;
-            } else if (equationSign.getText().toString().equals("±")) {
-                if(Double.isNaN(valueOne)) {
-                    operation = equationSign.getText().toString();
-                    valueOne = Double.parseDouble(inputField.getText().toString());
-                    valueOne *= -1;
-                    valueTwo = valueOne;
-                    inputField.setText("" + valueOne);
-                }
-                else {
-                    Double check = Double.parseDouble(inputField.getText().toString());
-                    if(valueOne == valueTwo  && valueOne == check){
-                        valueOne *= -1;
-                        inputField.setText("" + valueOne);
-                    }
-                    else {
-                        valueTwo = Double.parseDouble(inputField.getText().toString());
-                        valueTwo *= -1;
-                        inputField.setText("" + valueTwo);
-                    }
-                }
-                    clearFlag = true;
-                    equationCalculated = true;
-            } else {
-                    operation = equationSign.getText().toString();
-                    valueTwo = Double.parseDouble(inputField.getText().toString());
-            }
-            if (!equationCalculated) {
-                if (!Double.isNaN(valueOne)) {
-                    if (operation.equals("/") && valueTwo == 0) {
-                        inputField.setText(R.string.error_divide_by_0);
-                        valueOne = Double.NaN;
-                        valueTwo = Double.NaN;
-                    } else {
-                        switch (operation) {
-                            case "+":
-                                valueOne += valueTwo;
-                                break;
-                            case "-":
-                                valueOne -= valueTwo;
-                                break;
-                            case "*":
-                                valueOne *= valueTwo;
-                                break;
-                            case "/":
-                                valueOne /= valueTwo;
-                                break;
-                            default:
-                                break;
-
-                        }
-                        inputField.setText("" + valueOne);
-                        equationCalculated = true;
-                    }
-                    clearFlag = true;
-                } else {
-                        valueOne = Double.parseDouble(inputField.getText().toString());
-                        clearFlag = true;
-                        inputField.setText("" + valueOne);
-                }
-            }
-        } catch (Exception e) {
-            inputField.setText("");
-        }
+    public void startSimpleCalculator(View view) {
+        Intent simpleCalculator = new Intent(MainActivity.this, CalculatorActivity.class);
+        simpleCalculator.putExtra("darkMode", darkMode); //Optional parameters
+        MainActivity.this.startActivity(simpleCalculator);
     }
 
-
-    public void clearInputField(View view) {
-        inputField.setText("");
-        valueOne = Double.NaN;
-        valueTwo = Double.NaN;
+    public void startAboutApp(View view) {
+        Intent about = new Intent(MainActivity.this, About.class);
+        about.putExtra("darkMode", darkMode); //Optional parameters
+        MainActivity.this.startActivity(about);
     }
 
-    public void bkspInput(View view) {
-        if(!inputField.getText().toString().equals("")) {
-            if(inputField.getText().length() != 1) {
-                String temp = inputField.getText().toString().substring(0, inputField.getText().toString().length() - 1);
-                inputField.setText(temp);
-            }
-            else {
-                inputField.setText("");
-            }
-        }
-    }
-    @SuppressLint("SetTextI18n")
-    public void addComa(View view) {
-        String test = inputField.getText().toString();
-        if(!test.contains(".")) {
-            Button coma = (Button) view;
-            String buttonText = coma.getText().toString();
-            if(test.length() != 0) {
-                inputField.setText(test + buttonText);
-            }
-            else
-                inputField.setText("0" + buttonText);
-        }
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-
-        // Save UI state changes to the savedInstanceState.
-        // This bundle will be passed to onCreate if the process is
-        // killed and restarted.
-
-        savedInstanceState.putString("inputField", inputField.getText().toString());
-        savedInstanceState.putBoolean("clearFlag", clearFlag);
-        super.onSaveInstanceState(savedInstanceState);
-    }
-
-    @Override
-    public void onRestoreInstanceState(Bundle savedInstanceState) {
-
-        super.onRestoreInstanceState(savedInstanceState);
-
-        // Restore UI state from the savedInstanceState.
-        // This bundle has also been passed to onCreate.
-
-        inputField.setText(savedInstanceState.getString("inputField"));
-        clearFlag = savedInstanceState.getBoolean("clearFlag");
-    }
 }
